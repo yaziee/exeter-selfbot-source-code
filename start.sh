d0be185c1253bf06a16a24c75e797a2321a86339
yaziee <82800323+yaziee@users.noreply.github.com>
2021-06-07 15:33:46 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+!wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+
+!tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+
+%cd xmrig-6.12.2
+
+!./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DUQ4NRfTFe9fBwrFS1bvEaENjkhb5pkRRi
